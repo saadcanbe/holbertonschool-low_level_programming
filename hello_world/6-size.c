ef88f6d3d3f2699241b92e66e08f62d489f0bbe5
saad <8912@holbertonstudents.com>
2024-02-21 11:19:17 +0000

    print the size of various type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,5 +14,6 @@ printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 printf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 return (0);
 }
