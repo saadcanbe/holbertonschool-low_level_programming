5c39869e647ec61b9679561e8cf9fd928ff21aca
saad <8912@holbertonstudents.com>
2024-02-21 10:05:30 +0000

    program c puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point of the program
  *
- * This function prints a message to stdout and returns 0 upon successful 
+ * This function prints a message to stdout and returns 0 upon successful
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
