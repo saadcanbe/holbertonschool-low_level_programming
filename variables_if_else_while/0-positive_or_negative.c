24f87ca8feba3861b0cf8ca7688b2490f536eaf0
saad <8912@holbertonstudents.com>
2024-02-22 11:54:50 +0000

    pos neg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n>0)
 {
-printf("the number %d is positive\n", n);
+printf(" %d is positive\n", n);
 }
 else if (n==0)
 {
-printf("the number %d is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("the number %d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
