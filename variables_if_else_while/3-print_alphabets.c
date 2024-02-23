50af626a64389351232fbfa71d5e7a3b5e790ddc
saad <8912@holbertonstudents.com>
2024-02-23 23:17:07 +0000

    PINT UPPER AND LOWER CASE

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
- * Print the last digit of the number stored in the variable n
+ * Print upper and lowr case 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char i = 'a';
-	 char j = 'A';
-
-        while (i <= 'z' && j <= 'Z')
-        {
-         putchar(i);
-	 putchar(j);
-         i++;
-	 j++;
-        }
-        putchar('\n');
-        return (0);
+char i = 'a';
+char j = 'A';
+while (i <= 'z' && j <= 'Z')      
+{
+putchar(i);
+i++;
+if (i == 'z')
+{
+putchar(j);
+j++;
+}
+}
+putchar('\n');
+return (0);
 }
