ca9a7a5ed513a1d2effdf3594d13fb5158620161
saad <8912@holbertonstudents.com>
2024-02-23 23:57:18 +0000

    base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * number base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+while (i < 10 )
+{ 
+i;
+i++
+}
+printf('\n');
+return (i);
+}  
