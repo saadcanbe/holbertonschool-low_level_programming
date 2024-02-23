7db34e53da9965a245f655fa6fa7489086383cef
saad <8912@holbertonstudents.com>
2024-02-23 23:43:30 +0000

    alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Print upper and lowr case 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i = 'a';
+char j = 'A';
+while (i <= 'z' && j <= 'Z')
+{
+if (i !='q' && i!='e')
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
