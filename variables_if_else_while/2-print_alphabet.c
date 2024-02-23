516263bb9fca1b9fc6e5055bcd003b1e44151a51
saad <8912@holbertonstudents.com>
2024-02-23 17:08:24 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Print the last digit of the number stored in the variable n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{ 
+	 putchar(i);
+	 i++;
+	}
+	return (0);
+}
