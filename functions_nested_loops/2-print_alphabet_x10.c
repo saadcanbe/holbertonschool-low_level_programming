be152f7701b58587b9799b621a221986c3189b96
saad <8912@holbertonstudents.com>
2024-02-24 23:03:45 +0000

    alpha 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 #include "main.h"
 /**
  * main - Entry point
- * Print alph lower case
+ * print_alphabet_x10 
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 char c;
 int i;
-for(i=0 ; i < 10; i++)
+for (i = 0 ; i < 10; i++)
 {
 c = 'a';
 while (c <= 'z')
