8577a25dd5ead0b38728c171e7dafe501ac2f142
saad <8912@holbertonstudents.com>
2024-02-24 22:50:29 +0000

    alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Print alph lower case
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+for(i=0 ; i <= 10; i++)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+} 
