be0463fe33722187ad5c6e4f89b58857cce4b2d1
saad <8912@holbertonstudents.com>
2024-02-24 21:36:33 +0000

    prototype

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Print alph lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i = 'a';
+while(i <= 'z')
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
