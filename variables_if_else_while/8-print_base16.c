0abbd71bf4142e4c2a38664799b8da1a1295f26e
saad <8912@holbertonstudents.com>
2024-02-24 01:20:18 +0000

    hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
- * Print inverse alphabet
+ * Print numbers in base 16
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 0 ; i <= 9 ; i++)
-{        
-putchar(i + '0');
+    int i;
+    for (i = 0; i <= 9; i++)
+    {
+        putchar(i + '0');
+    }
+
+    char j;
+    for (j = 'a'; j <= 'f'; j++)
+    {
+        putchar(j);
+    }
+
+    putchar('\n');
+
+    return (0);
 }
-char j;
-for (j = 'a' ; j <= 'f' ; j++)
-{
-putchar(j);
-}
-putchar('\n');
-return (0);
-} 
+
