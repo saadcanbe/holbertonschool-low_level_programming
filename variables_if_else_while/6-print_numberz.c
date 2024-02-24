16ae0f2ee2d51b6aecd0ddf7be68bdc76376a67e
saad <8912@holbertonstudents.com>
2024-02-24 00:20:24 +0000

    putchar number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * number base 10 with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i == 0; i <= 9; i++)
+{
+putchar(i + 'O');
+}
+putchar('\n');
+return (0);
+}
