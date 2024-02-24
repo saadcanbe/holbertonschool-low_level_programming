5d2f5cbe9a418fef9d271690356508e732308496
saad <8912@holbertonstudents.com>
2024-02-24 11:34:09 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Print _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char tmp = _putchar;
+int i = 0;
+while (tmp[i] != \0)
+{ putchar(tmp[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
