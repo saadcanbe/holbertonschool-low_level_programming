ff807b524f86fd94fd30629186ff4151e5a28685
saad <8912@holbertonstudents.com>
2024-02-24 01:08:52 +0000

    hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 int i;
-char j;
 for (i = 0 ; i <= 9 ; i++)
 {        
 putchar(i + '0');
 }
-for (j = 'a' ; j <= 'f' ; j++)
+for (char j = 'a' ; j <= 'f' ; j++)
 {
 putchar(j);
 j++;
