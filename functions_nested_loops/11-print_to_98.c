8e0e3da872835b69d2012a1935952d0d883e9ae1
saad <8912@holbertonstudents.com>
2024-02-25 22:13:08 +0000

    n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,40 @@ if (n <= 98)
 {
 for (i = n ; i <= 98 ; i++)
 {
-_putchar(i + '0');
 if (i != 98)
 {
 _putchar(',');
 _putchar(' ');
 }
+if (i >= 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+else
+{
+_putchar(i + '0');
 }
 }
 else
 {
 for (i = n ; i >= 98 ; i--)
 {
-_putchar(i + '0');
 if (i != 0)
 {
 _putchar(',');
 _putchar(' ');
+}
+if (i >= 100)
+{
+_putchar((n / 100) + '0');
+_putchar((n / 10) % 10 + '0');
+_putchar((i % 10) + '0');
+}
+else
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 }
 }
 }
