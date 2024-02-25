39a8db9fc297f8ed1d18aeb1c5c5eb63bcde49d2
saad <8912@holbertonstudents.com>
2024-02-25 14:02:07 +0000

    hours of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Print 00:00 to 23:59
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+for (i = 0 ; i < 24 ; i++)
+{
+for (j = 0 ; j < 60 ; j++)
+{
+_putchar(i / 10 +'0');
+_putchar(i % 10 +'0');
+_putchar(':'):
+_putchar(j / 10 +'0');
+_putchar(j % 10 +'0');
+}
+}
