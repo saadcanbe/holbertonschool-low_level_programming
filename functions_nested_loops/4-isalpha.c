e5fe7fcfa38e5e90b77b3ba6b77cc3bf5979a356
saad <8912@holbertonstudents.com>
2024-02-25 01:31:20 +0000

    check alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * check alpha
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
