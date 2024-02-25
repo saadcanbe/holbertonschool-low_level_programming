df596409c121b095a91b17ecca8c7997d9ca088d
saad <8912@holbertonstudents.com>
2024-02-25 14:08:52 +0000

    hours of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,8 +16,10 @@ for (j = 0 ; j < 60 ; j++)
 {
 _putchar(i / 10 +'0');
 _putchar(i % 10 +'0');
-_putchar(':'):
+_putchar(':');
 _putchar(j / 10 +'0');
 _putchar(j % 10 +'0');
+_putchar('\n');
+}
 }
 }
