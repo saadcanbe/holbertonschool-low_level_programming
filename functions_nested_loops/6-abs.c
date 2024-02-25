2df6fb75c062ddda3f96ce65c05a5457f757b6d3
saad <8912@holbertonstudents.com>
2024-02-25 02:00:37 +0000

    absolut value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,24 +4,17 @@
 #include "main.h"
 /**
  * main - Entry point
- * Print sign of n
+ * Print abolut value
  * Return: Always 0 (Success)
  */
-int print_sign(int n)
+int _abs(int n)
 {
 if (n > 0)
 {
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
+return (n);
 }
 else
 {
-_putchar('-');
-return (-1);
+return (-n);
 }
 }
