3f2e467658d184f75e0299172c57f3ee28e8e6ca
saad <8912@holbertonstudents.com>
2024-02-26 10:57:37 +0000

    0 to 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * print 0 to 14 ten times
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void);
+{
+int i, j;
+for (i = 0 ; i <= 10 ; i++)
+{
+for (j = 0 ; i <= 14 ; j++)
+{
+_putchar(j +'0');
+}
+}
+_putchar('\n');
+}
