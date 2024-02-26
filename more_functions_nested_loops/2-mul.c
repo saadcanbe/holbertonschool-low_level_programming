e8d2b94b06490e0b1123d4c4e1bce991804750e2
saad <8912@holbertonstudents.com>
2024-02-26 09:54:15 +0000

    multiplication a b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * do pultiplicaton
+ * mutlipication a b 
+ *
+ * Return: 0 succes
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
