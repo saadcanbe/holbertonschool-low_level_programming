46236390563b2798e68daa38b8cca8fa45b26290
saad <8912@holbertonstudents.com>
2024-02-26 10:15:08 +0000

    print 0 to 9 except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * check c upper case
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+}
