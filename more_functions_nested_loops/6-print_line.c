0cf85068ddf200559fc9995bbf3ce4c963706657
saad <8912@holbertonstudents.com>
2024-02-27 09:14:48 +0000

    print a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * print 0 to 14 ten times
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0 ; i <= n ; i++)
+{
+if (n = 0)
+{
+_putchar('\n');
+}
+else 
+{
+_putchar('_');
+}
+}
+}
