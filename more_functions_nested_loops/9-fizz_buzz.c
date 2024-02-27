f3c0d589829a1cfa64c742cb0a90c33bbc496629
saad <8912@holbertonstudents.com>
2024-02-27 10:59:38 +0000

    fizz buzz quiz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - Print a square of a specified size using the '#' character.
+ * @size: The size of the square.
+ */
+int main ()
+{
+int i;
+for (i = 1; i <= 100; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+           _putchar('F');
+           _putchar('i');
+           _putchar('z');
+           _putchar('z');
+           _putchar('B');
+           _putchar('u');
+           _putchar('z');
+           _putchar('z');
+           _putchar(' ');
+        } else if (i % 3 == 0) {
+           _putchar('F');
+           _putchar('i');
+           _putchar('z');
+           _putchar('z');
+           _putchar(' ');
+        } else if (i % 5 == 0) {
+           _ putchar('B');
+           _ putchar('u');
+           _ putchar('z');
+           _ putchar('z');
+           _ putchar(' ');
+	}
+	 else
+        {
+	_putchar(i + '0');
+	_putchar(' ');
+	}
+	_putchar('\n');
+}
