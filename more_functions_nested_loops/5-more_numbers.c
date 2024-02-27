0fe93dd67c042b3bb0b46f12c49687116679227b
saad <8912@holbertonstudents.com>
2024-02-27 08:55:28 +0000

    0 to 14 ten times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,15 @@ for (i = 0 ; i < 10 ; i++)
 {
 for (j = 0 ; j < 15 ; j++)
 {
+if (j < 10)
+{
 _putchar(j + '0');
+}
+else
+{
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
 }
 _putchar('\n');
 }
