698bb0660458d765c5194bf259f8e70f03b80944
saad <8912@holbertonstudents.com>
2024-02-27 10:09:16 +0000

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - Prints a line of underscores.
+ * @n: The number of underscores to print.
+ */
+void print_square(int size);
+{
+int i, j;
+for (i = 0 ; i <= n ; i++)
+{
+for (j = 0 ; j <= n ; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
