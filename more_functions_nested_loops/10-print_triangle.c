2035a6b07d3d29067ad3b89f662e5a8bfa9e5f53
saad <8912@holbertonstudents.com>
2024-02-28 14:45:13 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,20 +5,25 @@
  * print_triangle - Prints a triangle of a specified size.
  * @size: The size of the triangle.
  */
-void print_triangle(int size) {
+void print_triangle(int size)
+{
 int i, j;
-    if (size <= 0) {
-        _putchar('\n');
-        return;
-    }
-    for (i = size; i > 0; i--) {
-        for (j = 0; j < size ; j++) {
-	if ( j < i-1 )
-	{    _putchar(' ');
-	}
-	else
-        _putchar('#');
-	}
-        _putchar('\n');
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = size; i > 0; i--)
+{
+for (j = 0; j < size ; j++)
+{
+if ( j < i-1 )
+{
+_putchar(' ');
+}	
+else
+_putchar('#')
+}
+_putchar('\n');
+}
 }
