a76e39de710aa1f080fc21d9c59e593c1b3cf16e
saad <8912@holbertonstudents.com>
2024-02-28 15:45:47 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,27 +6,24 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
-{
-printf("FizzBuzz ");
+{i
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
-{
-if (i == 100)
 {
 printf("%d" , i);
 }
-else
+if (i < 100)
 {
-printf("%d " , i);
-}
+printf(" ");
 }
 }
 printf("\n");
