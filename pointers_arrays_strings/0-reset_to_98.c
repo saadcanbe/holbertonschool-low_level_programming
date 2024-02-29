d4e4802327c114ad22c89f725d467e2ff41c925f
saad <8912@holbertonstudents.com>
2024-02-29 09:28:46 +0000

    set pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * rest_to_98
+ * @n: The character to be updated
+ *
+ * Return:0 (success)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
