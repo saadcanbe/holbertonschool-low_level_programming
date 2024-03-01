3008150d239348673cefaef43d95b9fb414812b5
saad <8912@holbertonstudents.com>
2024-03-01 11:20:02 +0000

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * _ - print a string
+ * @str:pointer to the string
+ * Return:0 success
+ */
+void _puts(char *str)
+{
+while (*s != '\0')
+{
+s++;
+}
+s--;
+while (s >=0 && *s != '\0')
+{_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
