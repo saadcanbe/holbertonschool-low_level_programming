6a2958c9e2e36d678ea8988f5dec447f199b8384
saad <8912@holbertonstudents.com>
2024-03-01 09:23:29 +0000

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
 /**
  * _strlen - compute a lenght of a string
  * @s:pointer to the string
- * Return the lenght of the string
+ * Return: the lenght of the string
  */
 int _strlen(char *s)
 {int lenght = 0;
