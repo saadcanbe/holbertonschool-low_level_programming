14b2940c02dde2dd8945d51dcf615c5fd62ac7b4
saad <8912@holbertonstudents.com>
2024-03-02 21:12:17 +0000

    put half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * put_half - print the half
+ * @str: Pointer to the string.
+ */
+void puts_half(char *str);
+{
+int lenght = strlen(str);
+int index;
+if ( lenght % 2 == 0)
+{index = lenght / 2
+}
+else
+{
+index = (lenght - 1) / 2;
+}
+for (i = index ; i < lenght ; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
