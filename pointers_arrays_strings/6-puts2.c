a211cab7a795d4210d9a99eee6fd5e72aad40c96
saad <8912@holbertonstudents.com>
2024-03-02 20:48:35 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: Pointer to the string to be reversed.
+ */
+void puts2(char *str)
+{
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
