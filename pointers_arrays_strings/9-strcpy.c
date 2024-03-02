6e0f6944b08a06b965fd1eb9b06fe7f327040494
saad <8912@holbertonstudents.com>
2024-03-02 23:57:40 +0000

    copy string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copy the string
+ * @dest @src: Pointer to the string.
+ * Return: copy the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0 ; src[i] != '\0' ; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
