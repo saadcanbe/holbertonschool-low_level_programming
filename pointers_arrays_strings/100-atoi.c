fa0da8e0a0de1c5c7048c08b27bbc60712e61837
Your Name <8912@holbertonstudents.com>
2024-03-03 13:18:16 +0000

    convert

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,14 +11,12 @@ int sign = 1;
 int result = 0;
 int i = 0;
 while (s[i] == ' ')
-{
 i++;
 while (s[i] == '-' || s[i] == '+')
 {
 if (s[i] == '-')
 sign *= -1;
 i++;
-}
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
